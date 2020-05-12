29caa62e5e47f947a01c9be53efdb41f8e3b0448
OreAkintobi <oreakintobi@gmail.com>
2020-05-12 11:42:39 +0100

    Updates

---
+++ precache-manifest.0f603189f3c86a765e6bff07a750cfdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254cc4aef2f940d1281a9763dd06f5f5",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "5ed4e1905493238f20a9",
+    "url": "/portfolio-react/static/css/main.f03c8f84.chunk.css"
+  },
+  {
+    "revision": "dcabdb3c0d44f2f71658",
+    "url": "/portfolio-react/static/js/2.5e661e60.chunk.js"
+  },
+  {
+    "revision": "5ed4e1905493238f20a9",
+    "url": "/portfolio-react/static/js/main.09a953bf.chunk.js"
+  },
+  {
+    "revision": "a47c9b6077030111fb2a",
+    "url": "/portfolio-react/static/js/runtime-main.38d0b6cc.js"
+  }
+]);
